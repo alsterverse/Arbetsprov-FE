938bab7266bb191b5e6effcba969890b3bb578e2
josefinljung <58325534+josefinljung@users.noreply.github.com>
2021-05-28 20:24:41 +0200

    Fixed localstorage error for other browsers and some styling issues.

--- weather-app/src/components/Searchbar/Searchbar.js
+++ weather-app/src/components/Searchbar/Searchbar.js
@@ -4,9 +4,6 @@ import React from 'react';
 const Searchbar = ({search, setSearch, getData, createItem }) => {
 
 return(
-
-
-    
     <div className="Searchbar">
       <h1>Hur är vädret i...</h1>
       <form id="InputContainer" onSubmit={getData}>
@@ -15,19 +12,14 @@ return(
         placeholder="Plats:" 
         name="city"
         id="searchInput" 
+        spellCheck="false"
         value={search} 
         onChange={e => setSearch(e.target.value)}
         onKeyPress={e => e.key === 'Enter'|| e.keyCode ==='13' ? getData : null} />
         <button type="button" id="SaveSearch" onClick={createItem}>+</button>
       </form>
-      {/* <button id="SearchButton">Get Data</button> */}
-      {/* <div>{location}</div>
-      <div>{temperature}</div>
-      {(rain == "Rain") ? <div>{rain}</div>
-      : null } */}
     </div>
-
-  )
-}
+  );
+};
 
 export default Searchbar;
--- weather-app/src/components/Searchresult/Searchresult.js
+++ weather-app/src/components/Searchresult/Searchresult.js
@@ -1,8 +1,8 @@
 import './Searchresult.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import DeleteIcon from '../../assets/CloseButton.svg';
 
-const SearchResult = ({savedWeatherData, weatherData, error, removeItem}) => {
+const SearchResult = ({weatherData, error, removeItem}) => {
 
   let storedWeatherData = localStorage.getItem('WeatherSearch');
   let parsedWeatherData = JSON.parse(storedWeatherData);
@@ -11,18 +11,12 @@ const SearchResult = ({savedWeatherData, weatherData, error, removeItem}) => {
     <div className="Searchresult">
             {weatherData !== null ? (
               <React.Fragment>
-                <div className={`WeatherItem 
+                <div className={`WeatherItem  
                   ${(weatherData.current.temperature >= '20') ? `hightemperature` : ''}
                   ${(weatherData.current.temperature <= '19') ? `mediumtemperature` : ''}
                   ${(weatherData.current.temperature <= '0') ? `lowtemperature` : ''}
                   ${(weatherData.current.weather_descriptions == "Rain") ? `raining` : ''}`                    
                   }>     
-                  {/* <div className={`WeatherIcon
-                  
-                  {if (${(weatherData.current.weather_descriptions == 'Sunny') `sun`) else ( {`fallback`})
-
-                  `} /> */}
-
                   <div className={`WeatherIcon ${(weatherData.current.weather_descriptions)}`} />
                   <div>
                     <div className="Degrees">{weatherData.current.temperature}</div>
@@ -32,25 +26,25 @@ const SearchResult = ({savedWeatherData, weatherData, error, removeItem}) => {
               </React.Fragment>
             ) : ( <p>{error}</p> )
           }
-          {parsedWeatherData != null || parsedWeatherData.length >= 0 ? 
+          {parsedWeatherData && 
               (parsedWeatherData.map((weatherCard, i) => {
-                return(
-                <div key={weatherCard[i]} className={`WeatherItem 
-                ${(weatherCard.current.temperature >= '20') ? `hightemperature` : ''}
-                ${(weatherCard.current.temperature <= '19') ? `mediumtemperature` : ''}
-                ${(weatherCard.current.temperature <= '0') ? `lowtemperature` : ''}
-                ${(weatherCard.current.weather_descriptions == "Rain") ? `raining` : ''}`                    
-              }>     
-                <div className={`WeatherIcon ${(weatherCard.current.weather_descriptions)}`} />
-                <div>
-                  <div className="Degrees">{weatherCard.current.temperature}</div>
-                  <div className="NameOfCity">{weatherCard.location.name}</div>
-                </div>
-                <button type="button" className="DeleteIcon" onClick={e => removeItem(i)}>
-                  <img src={DeleteIcon} alt="DeleteIcon" />
-                </button>
-              </div>)
-              })) : ''
+                return (
+                  <div key={weatherCard[i]} className={`WeatherItem 
+                    ${(weatherCard.current.temperature >= '20') ? `hightemperature` : ''}
+                    ${(weatherCard.current.temperature <= '19') ? `mediumtemperature` : ''}
+                    ${(weatherCard.current.temperature <= '0') ? `lowtemperature` : ''}
+                    ${(weatherCard.current.weather_descriptions == "Rain") ? `raining` : ''}`                    
+                    }>     
+                    <div className={`WeatherIcon ${(weatherCard.current.weather_descriptions)}`} />
+                    <div>
+                      <div className="Degrees">{weatherCard.current.temperature}</div>
+                      <div className="NameOfCity">{weatherCard.location.name}</div>
+                    </div>
+                    <button type="button" className="DeleteIcon" onClick={e => removeItem(i)}>
+                      <img src={DeleteIcon} alt="DeleteIcon" />
+                    </button>
+                  </div>)
+              }))
           }
     </div>
   );
