5addebb014d7f7ef214c286a2f0b8c04ff66f8c8
Ariel Blomkvist Rova <ariel.blomkvist@motivactr.com>
2021-09-16 19:21:51 +0200

    sorting function

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,3 +1,7 @@
+import { CityData } from "./interfaces";
+
+// Den här är för jobbig att type:a! :D sry!
+// Finns det något smart sätt?
 export const formatCityData = (city: any) => {
     
     return { 
@@ -7,4 +11,12 @@ export const formatCityData = (city: any) => {
             weatherDesc: city.current.weather_descriptions[0], 
             weatherIcon: city.current.weather_icons[0]
         };
+}
+
+export const sortCities = (cities: CityData[]) => {
+    let sortedCities = cities.sort((a: any, b: any) => {
+        return a.temperature - b.temperature;
+    })
+
+    return sortedCities;
 }
