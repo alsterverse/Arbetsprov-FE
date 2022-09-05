0949ed81b0fae4d7c161356c1c9618a23f47dd98
LeoBergqvist <57221181+LeoBergqvist@users.noreply.github.com>
2022-09-05 13:00:37 +0200

    First commit
    Arbetsprov för Android

---
+++ app/src/main/java/com/example/composetest/ui/theme/Type.kt
@@ -0,0 +1,42 @@
+package com.example.composetest.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.composetest.R
+
+// Set of Material typography styles to start with
+
+val robotoFonts = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black),
+    Font(R.font.roboto_regular, FontWeight.Normal)
+)
+
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+
+    h1 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    )
+
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
